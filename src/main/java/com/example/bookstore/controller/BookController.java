c26352f1f051879bd6800af40a0aa4d79ade8568
Damian <kalbarzd@gmail.com>
2024-02-12 20:49:27 +0100

    finish jwt

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/api")
+@CrossOrigin
 @RestController
 public class BookController {
 
---
+++ src/main/java/com/example/bookstore/models/Role.java
@@ -0,0 +1,6 @@
+package com.example.bookstore.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
